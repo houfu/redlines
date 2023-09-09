96c163274f2acf5e8d785180819dbd1618773999
houfu <houfu@users.noreply.github.com>
2023-09-10 01:08:09 +0800

    Add Document Abstract Base Class Add PlainTextFile Document class
    Signed-off-by: houfu <houfu@users.noreply.github.com>

---
+++ tests/test_document.py
@@ -0,0 +1,17 @@
+from redlines import Redlines
+
+
+def test_PlainTextFile_text():
+    from redlines.document import PlainTextFile
+
+    source = PlainTextFile("documents/PlainTextFile/source.txt")
+    assert source.text == "The quick brown fox jumps over the lazy dog."
+
+    test = PlainTextFile("documents/PlainTextFile/test.txt")
+    assert test.text == "The quick brown fox walks past the lazy dog."
+
+    redline = Redlines(source, test)
+    assert (
+        redline.output_markdown
+        == "The quick brown fox <span style='color:red;font-weight:700;text-decoration:line-through;'>jumps over </span><span style='color:green;font-weight:700;'>walks past </span>the lazy dog."
+    )
